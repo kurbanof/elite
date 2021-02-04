9bd6972a6bad0ce5bb96829954c6e96ff7660774
muscleman-ak <muscleman.ak@gmail.com>
2021-02-04 23:15:43 +0700

    added: html for hero section

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 let menuBtn = document.querySelector('.burger');
 let menuList = document.querySelector('.menu');
+let bodyLock = document.querySelector('body');
+
 menuBtn.addEventListener('click', function () {
   menuBtn.classList.toggle('burger__active');
   menuList.classList.toggle('menu__active');
-
 });
+
+// document.addEventListener('keydown', function (event) {
+//   if (event.code == 'esc') {
+//     menuBtn.removeClass('burger__active');
+//   }
+// });
