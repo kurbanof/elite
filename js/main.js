39ffbf02c35e94a0047386951f1e69f63ed089b0
kurbanof <muscleman.ak@gmail.com>
2021-02-11 00:42:53 +0700

    added: trending section at all

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// mobile menu
 let menuBtn = document.querySelector('.burger');
 let menuList = document.querySelector('.menu');
 let bodyLock = document.querySelector('body');
@@ -7,8 +8,24 @@ menuBtn.addEventListener('click', function () {
   menuList.classList.toggle('menu__active');
 });
 
-// document.addEventListener('keydown', function (event) {
-//   if (event.code == 'esc') {
-//     menuBtn.removeClass('burger__active');
-//   }
-// });
+// trending tabs
+document.querySelectorAll('.tabs-trigger__item').forEach((item) =>
+  item.addEventListener('click', function (e) {
+    e.preventDefault();
+    const id = e.target.getAttribute('href').replace('#', '');
+
+    document.querySelectorAll('.tabs-trigger__item').forEach(
+      (child) => child.classList.remove('tabs-trigger__item_active')
+    );
+
+    document.querySelectorAll('.tabs-content__item').forEach(
+      (child) => child.classList.remove('tabs-content__item_active')
+    );
+
+    item.classList.add('tabs-trigger__item_active');
+    document.getElementById(id).classList.add('tabs-content__item_active');
+
+  })
+);
+
+document.querySelector('.tabs-trigger__item').click();
